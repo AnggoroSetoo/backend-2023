292971f3fcf1db1f201fd08b6c68814344d04d98
AnggoroSetoo <angg22016ti@student.nurulfikri.ac.id>
2023-10-20 08:45:25 +0700

    pertemuan-3-AddNameNIM

--- pertemuan-3/Animal.php
+++ pertemuan-3/Animal.php
@@ -1,6 +1,6 @@
 <?php
 
-# membuat class Animal
+# class Animal
 class Animal
 {
     # property animals
@@ -14,10 +14,10 @@ class Animal
         $this->animals = $data;
     }
 
-    # method index - menampilkan data animals
+    # showAll
     public function index()
     {
-        # Gunakan foreach untuk menampilkan data animals (array) satu per satu.
+        # menggunakan foreach untuk menampilkan data animals (array) satu per satu.
         foreach ($this->animals as $animal) {
             echo "$animal <br>";
         }
@@ -25,27 +25,27 @@ class Animal
         return $this; # Kembalikan objek saat selesai
     }
 
-    # method store - menambahkan hewan baru
+    # store/add
     # parameter: hewan baru
     public function store($data)
     {
         # Method array_push untuk menambahkan data baru ke dalam array $animals.
         array_push($this->animals, $data);
 
-        return $this; # Kembalikan objek saat selesai
+        return $this;
     }
 
-    # method update - mengupdate hewan
+    # update
     # parameter: index dan hewan baru
     public function update($index, $data)
     {
         # Mengganti data di indeks tertentu dengan data baru.
         $this->animals[$index] = $data;
 
-        return $this; # Kembalikan objek saat selesai
+        return $this;
     }
 
-    # method delete - menghapus hewan
+    # delete
     # parameter: index
     public function destroy($index)
     {
