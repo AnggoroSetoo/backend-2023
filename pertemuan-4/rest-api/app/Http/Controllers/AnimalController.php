2da38dc74b14badcc87d21f92d52b8caf7723dac
AnggoroSetoo <angg22016ti@student.nurulfikri.ac.id>
2023-10-20 11:22:37 +0700

    pertemuan-4

---
+++ pertemuan-4/rest-api/app/Http/Controllers/AnimalController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    public $animals = [
+        ["name" => "panda"],
+        ["name" => "nyamuk"],
+        ["name" => "singa"],
+        ["name" => "harimau"],
+        ["name" => "macan"],
+        ["name" => "kucing"],
+        ["name" => "ayam"]
+    ];
+
+    # method index - menampilkan data animals
+    public function index()
+    {
+        foreach ($this->animals as $animal) {
+            echo "Nama Hewan : $animal[name] <br>";
+        }
+    }
+
+    # method store - menambahkan hewan baru
+    public function store(Request $request)
+    {
+        array_push($this->animals, $request);
+        $this->index();
+        echo "Data Hewan Berhasil Ditambahkan";
+    }
+
+    # method update - mengupdate hewan
+    public function update(Request $request, $id)
+    {
+        $this->animals[$id] = $request;
+        $this->index();
+    }
+
+    # method destroy - menghapus hewan
+    public function destroy($id)
+    {
+        # gunakan method unset atau array_splice untuk menghapus data array
+        unset($this->animals[$id]);
+        $this->index();
+
+        echo "Data Hewan Berhasil Dihapus";
+    }
+}
